27756945cda503f7b2853cc9ea7032778ab5d94d
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2025-05-15 00:35:52 +0100

    feat : create base components

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ebank-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
