7651908841c8ee29c08470453cdf7000c54b5249
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2025-05-15 01:34:19 +0100

    feat: add daisyUi, husky and remix ui & header, footer component ui

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  currentYear!: number;
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
 }
