28cc8556f39d8752e607516545a85e7ea0716217
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2025-06-03 22:26:59 +0100

    feat : account http logic

--- src/app/dashboard/account/account.component.ts
+++ src/app/dashboard/account/account.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpService } from '../../core/service/http.service';
 
 @Component({
   selector: 'app-account',
   imports: [],
   templateUrl: './account.component.html'
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  http = inject(HttpService);
+
+  ngOnInit(): void {
+    this.http.get('accounts').subscribe({
+      next: (data) => {
+        console.log('Account data:', data);
+      },
+      error: (error) => {
+        console.error('Error fetching account data:', error);
+      }
+    });
+  }
 
 }
